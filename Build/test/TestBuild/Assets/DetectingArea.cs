9559cfffa5289ff37dc6e7cc5fcd025ea0602606
car_service_game <kosd2209@gmail.com>
2019-03-17 20:54:33 +0900

    문서 및 테스트 빌드 2019.03

---
+++ Build/test/TestBuild/Assets/DetectingArea.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectingArea : MonoBehaviour
+{
+    ObjectInfo objectInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectInfo = GetComponentInParent<ObjectInfo>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            Debug.Log("aa");
+            objectInfo._Button.SetActive(true);
+            objectInfo._time = 0f;
+        }
+    }
+}
---
+++ Build/test/TestBuild/Assets/Test/03.Scripts/PlayerCharacterInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region
+// 캐릭터의 정보를 담아 두는 스크립트입니다.
+#endregion
+
+namespace PlayerCharacter
+{
+    public class PlayerCharacterInfo : MonoBehaviour
+    {
+        [SerializeField] public static float s_CapsuleHeight = 1.6f;
+        [SerializeField] public static float s_CapsuleRadius = 0.3f;
+        [SerializeField] public static float s_CapsuleCenter = 0.8f;
+
+    }
+}
