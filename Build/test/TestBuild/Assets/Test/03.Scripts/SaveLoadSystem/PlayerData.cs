da814475c8dccccc29ac235b0ad1bf66100842b1
lalyns <blackalsl@gmail.com>
2019-03-18 23:07:13 +0900

    라이팅 더미맵

---
+++ Build/test/TestBuild/Assets/Test/03.Scripts/SaveLoadSystem/PlayerData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayerCharacter;
+
+[System.Serializable]
+public class PlayerData
+{
+    public float healthPoint;
+    public float[] position;
+
+    public PlayerData(PlayerCharacterInfo player)
+    {
+        healthPoint = PlayerCharacterInfo.HP;
+
+        position = new float[3];
+        position[0] = player.transform.position.x;
+        position[1] = player.transform.position.y;
+        position[2] = player.transform.position.z;
+    }
+}
---
+++ Build/test/TestBuild/Assets/TestMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    public float turnSpeed = 180f;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Update()
+    {
+        if(Input.GetAxis("Mouse X") > 0.2f)
+        {
+            this.transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+        }
+        else if(Input.GetAxis("Mouse X") < -0.2f)
+        {
+            this.transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetAxis("Mouse Y") > 0.2f)
+        {
+            Camera.main.transform.Rotate(-turnSpeed / 2f * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetAxis("Mouse Y") < -0.2f)
+        {
+            Camera.main.transform.Rotate(turnSpeed / 2f * Time.deltaTime, 0, 0);
+        }
+
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
